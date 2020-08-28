18d311a38d3cc8897388a02fe496be43fafb06ab
Abel Mendiola <abel.mendiola@comunidad.unam.mx>
2020-08-28 00:59:11 -0500

    Ejercicio 12 y 13 pacial

---
+++ ejercicio13.js
@@ -0,0 +1,28 @@
+
+/*
+Escriba una función de JavaScript para encontrar la 
+diferencia de dos matrices. 
+Ej difference([1, 2, 3], [100, 2, 1, 10]) Resultado --> ["3", "10", "100"]
+*/
+                                                                                                                                                                                                                                                                                                                                                                 
+longitudes = []
+function difference(matriz1, matriz2){
+    matriz1.sort()
+    matriz2.sort()
+    longitud1 = matriz1.length
+    longitudes.push(longitud1)
+    longitud2 = matriz2.length
+    longitudes.push(longitud2)
+    longitudes.sort(function(a, b){return a-b}
+    for(var i=0;i<matriz2.length; i++)
+}
+
+
+
+
+
+
+
+
+
+
--- ejercicio_12.js
+++ ejercicio_12.js
@@ -8,13 +8,28 @@ salida --> [4, 5, 8, 10, 12, 13]
 */
 
 longitudes = []
+matrizSuma = []
 function sumIndex(matriz1, matriz2){
     longitud1 = matriz1.length
     longitudes.push(longitud1)
     longitud2 = matriz2.length
     longitudes.push(longitud2)
-    for(var i=0; i<)
-    return matriz1, matriz2
+    longitudes.sort(function(a, b){return a-b})
+    for(var i=0; i<longitudes[0]; i++){
+        matrizSuma.push(matriz1[i]+matriz2[i])
+    }
+    if(longitud1 > longitud2){
+        adicionales1 = longitud1 - longitud2
+        for(var i=0; i<adicionales1; i++){
+            matrizSuma.push(matriz1[longitud2+i])
+        } 
+    } else if(longitud2>longitud1){
+        adicionales2 = longitud2 - longitud1
+        for(var i=0; i<adicionales2; i++){
+            matrizSuma.push(matriz2[longitud1+i])
+        } 
+    }
+    return matrizSuma
 }
 
 
@@ -22,10 +37,10 @@ const matrix1 = [1,0,2,3,4]
 const matrix2 = [3,5,6,7,8,13]
 console.log(sumIndex(matrix1, matrix2))
 
-
+/*
 const misNumeros = [1,10,2,5,7,3]
 misNumeros.sort(function(a, b){return a-b})
 console.log(misNumeros)
-
+*/
 
 
